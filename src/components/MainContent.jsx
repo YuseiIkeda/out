b046663d84d9ac34f70efff37d2b9ea4aebfe68c
YuseiIkeda <chys22020@g.nihon-u.ac.jp>
2023-10-21 16:16:13 +0900

    Birdwatching

---
+++ src/components/MainContent.jsx
@@ -0,0 +1,11 @@
+import WelcomeText from "./WelcomeText";
+import ImageSidebar from "./ImageSidebar";
+
+export default function MainContent() {
+    return (
+        <main>
+            <WelcomeText />
+            <ImageSidebar />
+        </main>
+    );
+}
